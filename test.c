f8a859cefa2dc4a2d10beb0c1c0f5d8b918b8d4e
Isaac Turner <spacey_sooty@outlook.com>
2024-02-07 13:12:57 +0800

    better test and add stdio.h to header

--- test.c
+++ test.c
@@ -2,5 +2,6 @@
 #include <utils.h>
 
 int main(void) {
+  eprintf("Hello, world!\n");
   return 0;
 }
--- utils.h
+++ utils.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <stdio.h>
+
 #ifdef __GNUC__
 #define eprintf(args...) fprintf(stderr, ##args)
 #else
